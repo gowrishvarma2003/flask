30a24fe929654447390a338115c3284ebd319449
gowrishvarma2003 <gowrishvarma@gmail.com>
2025-04-20 00:19:13 +0530

    add new comment

--- test.py
+++ test.py
@@ -85,3 +85,9 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+# To run the application, use the command: python test.py
+
+def test_signup(client):
+    return "User created successfully", 201
